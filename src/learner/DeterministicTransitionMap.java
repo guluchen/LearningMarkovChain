cd47c05bd4098cbbd0f2df37d133f9b56eb9ff93
yfc <gulu0724@gmail.com>
2017-06-30 23:34:01 +0800

    Reformat the code style

--- src/learner/DeterministicTransitionMap.java
+++ src/learner/DeterministicTransitionMap.java
@@ -1,64 +1,64 @@
 package learner;
 
 /**
-*
-* Copyright (c) 2017 Yu-Fang Chen
-* 
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-* 
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
-* 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
-* 
-*/
+ *
+ * Copyright (c) 2017 Yu-Fang Chen
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
 
 import java.util.HashMap;
 import java.util.Optional;
 
 public class DeterministicTransitionMap {
-	
-	private HashMap<String, StateAndFrequency> symbolToStateAndFrequencyMap ;
-	
-	public DeterministicTransitionMap(){
-		symbolToStateAndFrequencyMap = new HashMap<String, StateAndFrequency>();
-	};
-	
-	public void set(String symbol, State state, int frequency) {
-		symbolToStateAndFrequencyMap.put(symbol, new StateAndFrequency(state,frequency));
-	}
-	public Optional<StateAndFrequency> get(String symbol){
-		return Optional.ofNullable(symbolToStateAndFrequencyMap.get(symbol));
-	}
-	
-	public void remove(String symbol){
-		symbolToStateAndFrequencyMap.remove(symbol);
-	}
-	
-	public String toString(){
-		String ret="[";
-		for(String symbol:symbolToStateAndFrequencyMap.keySet()){
-			StateAndFrequency stateAndFrequency=
-					symbolToStateAndFrequencyMap.get(symbol);
-			if(stateAndFrequency!=null){
-				State state=stateAndFrequency.state;
-				int frequency=stateAndFrequency.frequency;
-				ret+=("--("+symbol+","+frequency+")-->"+state+" ");
-			}
-		}
-		ret+="]";
-		return ret;
-	}
-	
+
+  private HashMap<String, StateAndFrequency> symbolToStateAndFrequencyMap ;
+
+  public DeterministicTransitionMap(){
+    symbolToStateAndFrequencyMap = new HashMap<String, StateAndFrequency>();
+  };
+
+  public void set(String symbol, State state, int frequency) {
+    symbolToStateAndFrequencyMap.put(symbol, new StateAndFrequency(state,frequency));
+  }
+  public Optional<StateAndFrequency> get(String symbol){
+    return Optional.ofNullable(symbolToStateAndFrequencyMap.get(symbol));
+  }
+
+  public void remove(String symbol){
+    symbolToStateAndFrequencyMap.remove(symbol);
+  }
+
+  public String toString(){
+    String ret="[";
+    for(String symbol:symbolToStateAndFrequencyMap.keySet()){
+      StateAndFrequency stateAndFrequency=
+          symbolToStateAndFrequencyMap.get(symbol);
+      if(stateAndFrequency!=null){
+        State state=stateAndFrequency.state;
+        int frequency=stateAndFrequency.frequency;
+        ret+=("--("+symbol+","+frequency+")-->"+state+" ");
+      }
+    }
+    ret+="]";
+    return ret;
+  }
+
 }
--- src/learner/Main.java
+++ src/learner/Main.java
@@ -4,21 +4,18 @@ import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, AutomataDeterminismException {
-		//The main function demonstrate how to use this library
-		
-		SetToTreeShapedFFA test=new SetToTreeShapedFFA();
-		test.fromFile("./result.txt");
-		DeterministicFrequencyFiniteAutomata fpta=test.generateFPTA();
-		DeterministicFrequencyFiniteAutomataLearner learner=new DeterministicFrequencyFiniteAutomataLearner(fpta, 25, 0.2f);
-		learner.runAlergia();
-		DeterministicFrequencyFiniteAutomata result=learner.getFFA();
-		DeterministicFrequencyFiniteAutomataPrinter printer=new DeterministicFrequencyFiniteAutomataPrinter(result);
-		printer.printDTMCinPrism();
-		
-		
-		
+  public static void main(String[] args) throws IOException, AutomataDeterminismException {
+    //The main function demonstrate how to use this library
 
-	}
+    SetToTreeShapedFFA test=new SetToTreeShapedFFA();
+    test.fromFile("./result.txt");
+    DeterministicFrequencyFiniteAutomata fpta=test.generateFPTA();
+    DeterministicFrequencyFiniteAutomataLearner learner=new DeterministicFrequencyFiniteAutomataLearner(fpta, 25, 0.2f);
+    learner.runAlergia();
+    DeterministicFrequencyFiniteAutomata result=learner.getFFA();
+    DeterministicFrequencyFiniteAutomataPrinter printer=new DeterministicFrequencyFiniteAutomataPrinter(result);
+    printer.printDTMCinPrism();
+
+  }
 
 }
