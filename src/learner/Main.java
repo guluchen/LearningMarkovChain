98d006b43b57db5b97906c258d80560c7e568cb0
yfc <gulu0724@gmail.com>
2017-06-30 10:12:07 +0800

    Add a Main class to make it executable

---
+++ src/learner/Main.java
@@ -0,0 +1,24 @@
+package learner;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException, AutomataDeterminismException {
+		//The main function demonstrate how to use this library
+		
+		SetToTreeShapedFFA test=new SetToTreeShapedFFA();
+		test.fromFile("./result.txt");
+		DeterministicFrequencyFiniteAutomata fpta=test.generateFPTA();
+		DeterministicFrequencyFiniteAutomataLearner learner=new DeterministicFrequencyFiniteAutomataLearner(fpta, 25, 0.2f);
+		learner.runAlergia();
+		DeterministicFrequencyFiniteAutomata result=learner.getFFA();
+		DeterministicFrequencyFiniteAutomataPrinter printer=new DeterministicFrequencyFiniteAutomataPrinter(result);
+		printer.printDTMCinPrism();
+		
+		
+		
+
+	}
+
+}
